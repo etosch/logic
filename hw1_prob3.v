fef0871d40e6e1bf48caec18d4c4c76a895a0004
etosch <etosch@gmail.com>
2014-08-28 09:17:29 -0400

    forgot to save. new suitability

--- hw1_prob3.v
+++ hw1_prob3.v
@@ -6,19 +6,33 @@ Require Import PropLogic.
 Import ListNotations.
 
 (* define when two assign. agree on the atomic formulas *)
+Inductive in_assignment: atomic -> assignment -> Prop :=
+| in1 : forall a tv,
+          in_assignment a [(a, tv)]
+| in2 : forall a b c,
+          in_assignment a b -> in_assignment a (c::b).
+
 Inductive suitable_2 : formula -> assignment -> Prop :=
-  s1 : forall (a : assignment)  (atm : atomic) (tv : bool),
-         (In (atm, tv) a) -> suitable_2 (Atom atm) a
-| s2 : forall f a,
-         suitable_2 f a -> suitable_2 (Negation f) a
-| s3 : forall f g a,
-         suitable_2 f a -> suitable_2 g a -> suitable_2 (Disjunction f g) a.
+| s1 : forall atm a b, 
+         in_assignment atm a -> suitable_2 (Atom atm) a -> suitable_2 (Atom atm) (b::a)
+| s2 : forall f a b,
+         suitable_2 f a -> suitable_2 (Negation f) a -> suitable_2 (Negation f) (b::a)
+| s3 : forall f g a b,
+         suitable_2 f a /\ suitable_2 g a -> suitable_2 (Disjunction f g) a -> suitable_2 (Disjunction f g) (b::a).
+(*   s1 : forall (a : assignment)  (atm : atomic), *)
+(*          in_assignment atm a -> suitable_2 (Atom atm) a *)
+(* | s2 : forall f a b, *)
+(*          suitable_2 f a -> suitable_2 f (b::a). *)
+(* | s2 : forall f a, *)
+(*          suitable_2 f a -> suitable_2 (Negation f) a *)
+(* | s3 : forall f g a, *)
+(*          suitable_2 f a -> suitable_2 g a -> suitable_2 (Disjunction f g) a. *)
 
 Inductive agree : assignment -> assignment -> list atomic -> Prop :=
   a1 : forall a b,
          agree a b []
-| a2 : forall a b c d tv,
-         agree a b c -> (In (d, tv) a) /\ (In (d, tv) b) -> agree a b (d::c).
+| a2 : forall a b c d,
+         agree a b c -> in_assignment d a /\ in_assignment d b -> agree a b (d::c).
 
 Fixpoint atomicfs (f : formula) : list atomic :=
   match f with
@@ -26,6 +40,99 @@ Fixpoint atomicfs (f : formula) : list atomic :=
     | Negation f' => atomicfs f'
     | Disjunction f' g => atomicfs f' ++ atomicfs g
   end.
+
+Lemma all_formulae_have_atoms: forall F,
+                                 atomicfs F <> [].
+Proof. 
+  unfold not.
+  induction F; try (intros; inversion H).
+  apply IHF.
+  apply H1.
+  apply app_eq_nil in H1.
+  inversion H1.
+  generalize H0.
+  apply IHF1.
+Qed.
+
+Lemma empty_assignment_not_suitable: forall F,
+                                       suitable_2 F [] -> False.
+Proof. 
+  intros.
+  inversion H.
+Qed.  
+
+Lemma suitable_atomic_bidirectional: forall a atm,
+                                       suitable_2 (Atom atm) a -> in_assignment atm a.
+  induction atm.
+  intros.
+  inversion H.
+  apply H1.
+Qed.  
+
+Lemma atoms_negation_invariant: forall F,
+                                  atomicfs F = atomicfs (Negation F).
+Proof. 
+  induction F; simpl; reflexivity.
+Qed.
+
+Lemma atoms_disjunction_invariant: forall F G,
+                                     atomicfs F ++ atomicfs G = atomicfs (Disjunction F G).
+Proof. 
+  induction F; destruct G; simpl; reflexivity.
+Qed.
+
+
+Lemma negation_suitable_invariant: forall F a,
+                                     suitable_2 F a <-> suitable_2 (Negation F) a.
+Proof. 
+  induction F.
+  split; intros.
+
+  
+
+Lemma suitable_assignment_contains_all_atoms: forall F a atm,
+                                                suitable_2 F a -> In atm (atomicfs F) -> in_assignment atm a.
+Proof.
+  induction F.
+  intros.
+  apply suitable_atomic_bidirectional.
+  unfold atomicfs in H0.
+  inversion H0.
+  rewrite H1 in H.
+  apply H.
+  inversion H1. 
+  intros.
+  simpl in H0.
+  induction a; intros.
+  inversion H.
+  generalize H0.  
+
+
+
+  induction a.
+  intros.
+  inversion H.
+  inversion H1.
+  
+  induction F.
+  intros.
+  
+
+
+  
+
+Lemma suitable_extract : forall a F atm,
+                           suitable_2 F a -> (In atm (atomicfs F)) -> in_assignment atm a.
+Proof. 
+  intros.
+  induction F.
+  apply suitable_atomic_bidirectional in H.
+  simpl in H0.
+  inversion H0.
+  rewrite <- H1.
+  apply H.
+  inversion H1.
+  apply IHF.
 
 
 Theorem hw1_prob3 : forall a b F,
