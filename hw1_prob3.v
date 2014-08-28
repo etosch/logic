300f0f8c49f2b325099b173b0266a7036b1bbd2a
etosch <etosch@gmail.com>
2014-08-28 09:14:35 -0400

    trying out a new definition for suitability

--- hw1_prob3.v
+++ hw1_prob3.v
@@ -3,17 +3,32 @@ Require Import List.
 Require Import ListSet.
 Require Import PropLogic.
 
+Import ListNotations.
+
 (* define when two assign. agree on the atomic formulas *)
-Fixpoint get_all_atoms_assign (a : assignment) : set atomic :=
-  match a with
-    | nil => nil
-    | (b, truth_value)::t => b::(get_all_atoms_assign t)
+Inductive suitable_2 : formula -> assignment -> Prop :=
+  s1 : forall (a : assignment)  (atm : atomic) (tv : bool),
+         (In (atm, tv) a) -> suitable_2 (Atom atm) a
+| s2 : forall f a,
+         suitable_2 f a -> suitable_2 (Negation f) a
+| s3 : forall f g a,
+         suitable_2 f a -> suitable_2 g a -> suitable_2 (Disjunction f g) a.
+
+Inductive agree : assignment -> assignment -> list atomic -> Prop :=
+  a1 : forall a b,
+         agree a b []
+| a2 : forall a b c d tv,
+         agree a b c -> (In (d, tv) a) /\ (In (d, tv) b) -> agree a b (d::c).
+
+Fixpoint atomicfs (f : formula) : list atomic :=
+  match f with
+    | Atom atm => [atm]
+    | Negation f' => atomicfs f'
+    | Disjunction f' g => atomicfs f' ++ atomicfs g
   end.
 
-Fixpoint atoms_that_agree (a b : assignment) : set atomic :=
-  match a with
-    | nil => nil
-    | a0::ta =>  
 
 Theorem hw1_prob3 : forall a b F,
-                      (suitable F a) /\ (suitable F b) /\ (agree(a b F) = true) -> ((eval_formula F a) = (eval_formula F b)).  
+                      (suitable F a) /\ (suitable F b) /\ agree a b (atomicfs F) -> ((eval_formula F a) = (eval_formula F b)).  
+Proof. 
+  intros; induction F.
